9a75360be65b877bdba2d341e47233e767bc5a95
thomask902 <tkleinkn@uwaterloo.ca>
2024-10-04 12:22:10 -0400

    created .env with db url

--- server/server.py
+++ server/server.py
@@ -1,11 +1,22 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
 
 # need cors to allow other servers (our next.js server) to access the /api/home endpoint
 from flask_cors import CORS
 
+load_dotenv()
+
 # app instance
 app = Flask(__name__)
 CORS(app)
+
+# Set up the database URI (use environment variable)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 # /api/home
 @app.route("/api/home", methods=['GET'])
